dfcd577710cbd6dbc09d1d98bda3c870a181408c
Priyansh_R <priyanshratnala@gmail.com>
2026-08-02 16:10:05 -0400

    Updated the Difficulty and Themes. Now, each theme has 3 difficulties to choose from.

--- js/ui.js
+++ js/ui.js
@@ -1,27 +1,31 @@
 document
 .getElementById("shuffle")
-.onclick=function(){
+.onclick = function()
+{
+    // Shuffling the puzzle
 
     shuffle();
-
 };
 
 
+document
+.querySelectorAll("[data-theme]")
+.forEach(function(button)
+{
+    button.onclick = function()
+    {
+        // Changing the selected theme
+
+        changeTheme(button.dataset.theme);
+    };
+});
+
 
 document
-.querySelectorAll("[data-mode]")
-.forEach(button=>{
+.getElementById("difficulty")
+.onchange = function()
+{
+    // Changing the puzzle difficulty
 
-
-    button.onclick=function(){
-
-
-        changeMode(
-            button.dataset.mode
-        );
-
-
-    };
-
-
-});
+    changeDifficulty(this.value);
+};
